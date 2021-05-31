06530bde225c2bd4aaa2cd438e216b9328719b5d
LeeHsss <gustjr9630@naver.com>
2021-05-31 21:43:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aop_part4_chapter04/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aop_part4_chapter04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aop_part4_chapter04.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val mainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mainBinding.root)
+    }
+}
