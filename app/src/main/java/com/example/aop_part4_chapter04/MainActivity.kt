6bf08bab950c58b51126b3ad0a7b028623999d70
LeeHsss <gustjr9630@naver.com>
2021-06-01 21:40:17 +0900

    개같은거

--- app/src/main/java/com/example/aop_part4_chapter04/MainActivity.kt
+++ app/src/main/java/com/example/aop_part4_chapter04/MainActivity.kt
@@ -11,8 +11,28 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private var isGatheringAnimating : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mainBinding.root)
+
+        mainBinding.gatheringDigitalThingsLayout.setTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                isGatheringAnimating = true
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                isGatheringAnimating = false
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+            }
+        })
     }
 }
