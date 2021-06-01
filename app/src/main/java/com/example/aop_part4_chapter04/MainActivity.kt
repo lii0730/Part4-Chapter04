cc300f943666725b4418c3952b5ed77523730662
root <root@neodreams.co.kr>
2021-06-01 18:10:34 +0900

    어렵다

--- app/src/main/java/com/example/aop_part4_chapter04/MainActivity.kt
+++ app/src/main/java/com/example/aop_part4_chapter04/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.aop_part4_chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.aop_part4_chapter04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
